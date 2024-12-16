c5c5afa0c25b27c502700ed3063fa855b77bbce4
Montrelld <MOntrelld@nycstudents.net>
2024-12-16 09:22:11 -0500

    Part 15 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,16 @@ public class PlayerController : MonoBehaviour
     public InputAction MoveAction;
     private Rigidbody2D rigidbody2d;
     Vector2 move;
+    
+    
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,13 +30,36 @@ public class PlayerController : MonoBehaviour
     void Update()
     {   
          move = MoveAction.ReadValue<Vector2>();
-         Debug.Log(move) ;
+         
+
+         if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     void FixedUpdate()
     {
         Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
         rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
 }
 
